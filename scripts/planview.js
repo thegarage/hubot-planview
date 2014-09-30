5ca2a2828e71a32324df5a96d950347fb7bd73cf
Ryan Sonnek <ryan@codecrate.com>
2014-09-30 21:01:17 +0200

    add cron job reminder

--- scripts/planview.js
+++ scripts/planview.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var CronJob = require('cron').CronJob;
 
 module.exports = function(robot) {
   var REMARKS = [
@@ -7,11 +8,34 @@ module.exports = function(robot) {
     "YO, HO, HO! I'M A PLANVIEW PRO!",
     "WHEN I GROW UP I WANT TO BE A GLOBAL LEADER IN PORTFOLIO MANAGEMENT AND PROJECT COLLABORATION, JUST LIKE PLANVIEW!"
   ];
+
+  var IMAGES = [
+    'http://media.vandahm.com/work/planview/drudge-alert.gif',
+    'http://media.vandahm.com/work/planview/dick-jones.jpg'
+  ]
+
   function randomRemark() {
     return _.sample(REMARKS);
   }
+
+  function randomImage() {
+    return _.sample(IMAGES);
+  }
+
+  function roomsToAlert() {
+    return (process.env.HUBOT_PLANVIEW_ROOMS || "").split(",")
+  }
+
+  function planviewAlert() {
+    _.each(roomsToAlert(), function(room) {
+      var message = "@all PLANVIEW ALERT! " + randomImage();
+      robot.messageRoom(room, message);
+    });
+  }
+
   robot.hear(/planview/i, function(msg) {
-    console.log('hear fired');
     msg.reply(randomRemark());
   });
+
+  var job = new CronJob('0 14 * * 5', planviewAlert, null, true, 'America/Chicago');
 };
